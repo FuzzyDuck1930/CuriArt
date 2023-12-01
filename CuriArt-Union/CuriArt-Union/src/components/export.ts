308a01e0cda087344d84382efc12911be7815fbf
Anascalle <127751671+Anascalle@users.noreply.github.com>
2023-12-01 16:34:19 -0500

    Delete CuriArt-Union directory

--- CuriArt-Union/CuriArt-Union/src/components/export.ts
+++
@@ -1,4 +0,0 @@
-export {default as ArtistCard} from "./Card/card.js";
-export {default as Comments } from "./Comments/comments.js"
-export {default as Navegation} from "./Navbar/Navbar.js"
-export { default as Logobar} from "./Logobar/Logobar.js"
